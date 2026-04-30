c4785cdec601d2f9d0ef1be2c5df6fb27d47b3d1
Lautaro Cárdenas <lautarocardenas8@gmail.com>
2026-04-29 21:27:14 -0300

    Add sidenav and home basic structure

--- spacenova-front/src/app/app.routes.ts
+++ spacenova-front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+// import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'home',
+        loadChildren: () => import('./home/home.routes').then(m => m.routes)
+    }
+];
